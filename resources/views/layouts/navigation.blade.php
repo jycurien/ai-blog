05caf1dc1d39cbd978aa6a0af45ea74ec499cc8b
jycurien <curien.jeanyves@gmail.com>
2023-04-02 14:33:21 +0200

    Init and cleanup

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -26,9 +26,6 @@
                 <x-nav-link :href="route('login')" :active="request()->routeIs('login')">
                     {{ __('Login') }}
                 </x-nav-link>
-                <x-nav-link :href="route('register')" :active="request()->routeIs('register')">
-                    {{ __('Register') }}
-                </x-nav-link>
             </div>
             @else
 
@@ -105,9 +102,6 @@
             <div class="mt-3 space-y-1">
                 <x-responsive-nav-link :href="route('login')" :active="request()->routeIs('login')">
                     {{ __('Login') }}
-                </x-responsive-nav-link>
-                <x-responsive-nav-link :href="route('register')" :active="request()->routeIs('register')">
-                    {{ __('Register') }}
                 </x-responsive-nav-link>
             </div>
             @else
