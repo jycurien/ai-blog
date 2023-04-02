bffc9d7f01b3521470e61c4b788dd833395fe60b
jycurien <curien.jeanyves@gmail.com>
2023-04-02 15:02:08 +0200

    some ui fixes

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,7 +1,9 @@
 <x-app-layout>
     <x-slot name="header">
-        <div class="w-full bg-cover p-16 mb-6" style="background-image: url('{{ asset($post->image) }}'); background-position: center; height: 400px">
-            <h1 class="text-6xl font-bold mt-6 mb-6 text-center">{{ $post->title }}</h1>
+        <div class="w-full bg-cover -my-6" style="background-image: url('{{ asset($post->image) }}'); background-position: center; height: 400px">
+            <div class="flex items-center justify-center h-full bg-white bg-opacity-90 ">
+                <h1 class="font-bold text-6xl text-center text-orange-600 mx-16">{{ $post->title }}</h1>
+            </div>
         </div>
     </x-slot>
 
