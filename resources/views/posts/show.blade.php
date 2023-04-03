386bbeb16f46ab0806dd03ff3aab01a014cf3f90
jycurien <curien.jeanyves@gmail.com>
2023-04-03 12:20:34 +0200

    up show post template for better square image display

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -1,12 +1,14 @@
 <x-app-layout>
     <x-slot name="header">
-        <div class="w-full bg-cover bg-center -my-6" style="background-image: url('{{ asset($post->image) }}'); height: 400px">
-            <div class="flex items-center justify-center h-full bg-white bg-opacity-60 ">
-                <h1 class="font-bold text-6xl text-center text-orange-600 mx-16">{{ $post->title }}</h1>
-            </div>
+        <div class="text-center">
+            <h1 class="font-bold text-6xl text-orange-600 mx-16">{{ $post->title }}</h1>
         </div>
     </x-slot>
 
-    <div>{!! nl2br($post->content) !!}</div>
+    <div class="bg-white p-4 rounded shadow mt-6">
+        <img src="{{ asset($post->image) }}" alt="image for {{ $post->title }}" 
+            class="post-image m-auto md:float-left md:ml-0 md:mt-0 md:mr-4 md:mb-4" width="512" height="512">
+        {!! nl2br($post->content) !!}
+    </div>
 </x-app-layout>
 
