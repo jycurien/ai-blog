60a2a4d13d46f0316e95363be2469bc24e6ce4ce
jycurien <curien.jeanyves@gmail.com>
2023-04-04 15:28:35 +0200

    og image

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <meta name="csrf-token" content="{{ csrf_token() }}">
         <meta name="robots" content="index,follow">
+        <meta name="image" property="og:image" content="{{ url(asset('storage/og-card.jpg')) }}">
 
         <title>{{ config('app.name', 'Laravel') }} - {{ $title }}</title>
 
